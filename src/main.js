4dc3e9c165868f301b8afd0c68b968e225aa4b2d
Qianqian Hu <qianhu@pindous.com>
2018-03-23 17:20:37 -0700

    test API

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
-// import { weather } from './weather';
+// import { doctor } from './doctor';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $('#list').click(function() {
-    let museumName = $('#museumName').val();
-    $('#museumName').val("");
+  $('#doctorList').click(function() {
+    let medicalIssue = $('#medicalIssue').val();
+    $('#medicalIssue').val("");
 
     $.ajax({
       url:
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.6061,-122.3421&radius=5000&types=museum&name=${museumName}&key=AIzaSyDSFM9lXQaRZG4pnnUg5CtGyiT_Gjb0lCU`,
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.606,-122.332,1000&skip=2&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,
 
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        for(let i = 0; i < response.results.length; i++){
-          $('#name').text(`The name of ${museumName} is ${response.results[i].name}.`);
-          $('#address').text(`The location is ${response.results[i].vicinity}.`);
+        for(let i = 0; i < response.practices.length; i++){
+          $('#doctorList').append(`Doctor's list for ${medicalIssue} is ${response.practices[i].name}.`);
+          // $('#doctorInfo').text(`The location is ${response.results[i].vicinity}.`);
         }
       },
       error: function() {
