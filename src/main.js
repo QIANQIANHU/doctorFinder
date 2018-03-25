68604a5556f2221bd84542799be1f12f88f56a54
Qianqian Hu <qianhu@pindous.com>
2018-03-25 15:29:22 -0700

    get name of practice

--- src/main.js
+++ src/main.js
@@ -11,14 +11,15 @@ $(document).ready(function() {
 
     $.ajax({
       url:
-      `https://api.betterdoctor.com/2016-03-01/practices?query=${medicalIssue}&location=47.606,-122.332,1000&skip=2&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,
+      `https://api.betterdoctor.com/2016-03-01/practices?query=${medicalIssue}&location=47.606,-122.332,1000&skip=2&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,//searched by practice so that input issue key word can get all info
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
+        $('#doctorListResult').text("Seattle area's doctor's list for " + medicalIssue + " as below:")
         for(let i = 0; i <= response.data.length; i++){
-          $('#doctorListResult').append(`<br>Doctor's list for ${medicalIssue} is ${response.data[i].name}.`);
+          $('#doctorListResult').append(`<br><li> ${response.data[i].name}</li>`);
           // $('#doctorInfo').text(`The location is ${response.results[i].vicinity}.`);
         }
       },
