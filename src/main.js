5d00709777b6a24185f75c91f4ae337edd3078ea
Qianqian Hu <qianhu@pindous.com>
2018-03-25 14:39:05 -0700

    change the path to practices?, not doctor?

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
-var apiKey = require('./../.env').apiKey;
+// var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#doctorList').click(function() {
@@ -11,15 +11,14 @@ $(document).ready(function() {
 
     $.ajax({
       url:
-      `https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.606,-122.332,1000&skip=2&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,
-
+      `https://api.betterdoctor.com/2016-03-01/practices?query=${medicalIssue}&location=47.606,-122.332,1000&skip=2&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        for(let i = 0; i <= response.data.practices.length; i++){
-          $('#doctorList').append(`Doctor's list for ${medicalIssue} is ${response.data.practices[i].name}.`);
+        for(let i = 0; i <= response.data.length; i++){
+          $('#doctorListResult').append(`<br>Doctor's list for ${medicalIssue} is ${response.data[i].name}.`);
           // $('#doctorInfo').text(`The location is ${response.results[i].vicinity}.`);
         }
       },
