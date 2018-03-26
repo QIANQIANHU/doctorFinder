6944ef54a92c51833d2bb40ca98e959e3b863f37
Qianqian Hu <qianhu@pindous.com>
2018-03-26 11:32:11 -0700

    add doctor's details method

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
-// var apiKey = require('./../.env').apiKey;
+import './../.env';
+const apiKey = process.env.exports.apiKey;
+
 
 $(document).ready(function() {
   $('#doctorList').click(function() {
@@ -11,17 +13,19 @@ $(document).ready(function() {
 
     $.ajax({
       url:
-      `https://api.betterdoctor.com/2016-03-01/practices?query=${medicalIssue}&location=47.606,-122.332,100&skip=0&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,//searched by practice so that input issue key word can get all info
-      //
-      // https://api.betterdoctor.com/2016-03-01/practices?location=47.606,-122.332,100&skip=0&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43&query=head
+      `https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=47.606,-122.332,100&skip=0&limit=10&user_key=${apiKey}`,//searched by practice so that input issue key word can get all info
+
       data: {
         format: 'json'
       },
       success: function(response) {
-        $('#doctorListResult').text("Seattle area's doctor's list for " + medicalIssue + " as below:")
-        for(let i = 0; i <= response.data.length; i++){
-          $('#doctorListResult').append(`<br><li> ${response.data[i].uid}</li>`);
-          // $('#doctorInfo').text(`The location is ${response.results[i].vicinity}.`);
+        if (response.data.length === 0) {
+          $('#doctorListResult').text("Sorry, we didn't find doctors to your criteria!");
+        }else {
+          $('#doctorListResult').text("Seattle area's doctor's list for " + medicalIssue + " as below:")
+          for(let i = 0; i <= response.data.length; i++){
+            $('#doctorListResult').append(`<br><li> ${response.data[i].profile.last_name}</li>`);
+          }
         }
       },
       error: function() {
@@ -30,24 +34,27 @@ $(document).ready(function() {
     });
   });
 
-$('#doctorInfo').click(function() {
-  let doctorName = $('#doctorName').val();
-  $('#doctorName').val("");
+  $('#doctorInfo').click(function() {
+    let doctorName = $('#doctorName').val();
+    $('#doctorName').val("");
 
-  $.ajax({
-    url:
-    `https://api.betterdoctor.com/2016-03-01/doctors?location=47.606,-122.332,100&skip=0&limit=10&user_key=c3dfed4a07279a16d0b71a5dc66d5b43&name=${doctorName}&user_key=c3dfed4a07279a16d0b71a5dc66d5b43`,//searched by doctors so that input name  can get all name related info
+    $.ajax({
+      url:
+      `https://api.betterdoctor.com/2016-03-01/doctors?location=47.606,-122.332,100&skip=0&limit=10&name=${doctorName}&user_key=${apiKey}`,//searched by doctors so that input name  can get all name related info
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
-        $('#doctorNameInfo').text("Seattle area's doctor's name related with " + doctorName + " as below:")
-        for(let i = 0; i <= response.data.practices.length; i++){
-          $('#doctorNameInfo').append(`<br><li> ${response.data.practices[i].profile.name}</li>`);
-          // $('#doctorInfo').text(`The location is ${response.results[i].vicinity}.`);
-        }
-      },
+          if (response.data.length === 0) {
+            $('#doctorNameInfo').text("Sorry, we didn't find doctors to your criteria!");
+          }else {
+            $('#doctorNameInfo').text("Seattle area's doctors with " + doctorName + " in name have infomation below:")
+              for(let i = 0; i <= response.data.length; i++){
+                $('#doctorNameInfo').append(`<br><li> ${response.data[i].practices[0].phones[0].number}</li>`);
+                }
+              }
+            },
       error: function() {
         $('#errors').text("There was an error processing your request. Please try again.")
       }
